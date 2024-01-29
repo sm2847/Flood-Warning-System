8738917b512b98f9c4cc4732ec2ba75f7f0cc53f
Shanjeev Mathialagan <sm2847@cam.ac.uk>
2024-01-29 21:54:08 +0000

    Shanjeev

--- Task 1B.py
+++ Task 1B.py
@@ -1,23 +1,20 @@
 from floodsystem.geo import stations_by_distance
 from floodsystem.stationdata import build_station_list
 
-def change(x):
-    result = []
-
-    #Changes details of each station to list of tuples (station name, station town, distance)
-    for tuple in x:
-        result.append((tuple[0].name,tuple[0].town,tuple[1]))
-    return result
-
 def run():
     stations = build_station_list()
     stations_list = stations_by_distance(stations,(52.2053,0.1218))
     
-    changed_list = change(stations_list)
+    closest_stations = stations_list[:10]
+    furthest_stations = stations_list[-10:]
 
-    print(changed_list[:10])
+    for (station,distance) in closest_stations:
+        print((station.name,station.town,distance))
+    
     print("\n")
-    print(changed_list[-10:])
+    
+    for (station,distance) in furthest_stations:
+        print((station.name,station.town,distance))
 
 if __name__ == "__main__":
     run()
