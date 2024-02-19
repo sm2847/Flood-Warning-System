338a850bbff9585fe4401b69b658773e21baf9b1
Daniel Logan <dl725@cam.ac.uk>
2024-02-19 11:51:58 +0000

    2F Finished

--- Task2F.py
+++ Task2F.py
@@ -3,26 +3,12 @@ import matplotlib
 import datetime
 import matplotlib.pyplot as plt
 from floodsystem.datafetcher import fetch_measure_levels
-from plot import plot_water_level_with_fit
-<<<<<<< HEAD
-from floodsystem.stationdata import build_station_list
-=======
+from floodsystem.plot import plot_water_level_with_fit
 from floodsystem.stationdata import build_station_list, update_water_levels
->>>>>>> 80441f263c697c5117b32f7911f2000c276ae60c
 from floodsystem.flood import stations_highest_rel_level
 
 
 stations = build_station_list()
-<<<<<<< HEAD
-#top_five_risks =
-
-data = stations_highest_rel_level(stations, 5)
-
-
-
-
-
-=======
 update_water_levels(stations)
 
 def run():
@@ -30,39 +16,14 @@ def run():
 
         for station in stations_list:
                 dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=2))
-        
+
+                typ_low = [station.typical_range[0]] * len(dates)
+                typ_high = [station.typical_range[1]] * len(dates)
+                plt.title("Station = " + station.name)
+                plt.plot(dates, typ_low, label=("$Typical Low$"))
+                plt.plot(dates, typ_high, label=("$Typical High$"))
                 plot_water_level_with_fit(station, dates, levels, 4)
 
 if __name__ == "__main__":
     print("*** Task 2F: CUED Part IA Flood Warning System ***")
     run()
->>>>>>> 80441f263c697c5117b32f7911f2000c276ae60c
-
-
-
-
-
-
-
-
-
-
-
-
-#print(stations_highest_rel_level(stations, 0))
-
-#def run():
-#        for station, rel_level in top_five_risks:
-#                measure_id = station.measure_id
-#                dates, levels = fetch_measure_levels(measure_id, 2)
-#                plot_water_level_with_fit(station, dates, levels, 5)
-#        plt.show()
-#        return
-
-
-#print(top_five_risks)
-
-
-#if __name__ == "__main__":
-#    print("*** Task 2D: CUED Part IA Flood Warning System ***")
-#    run()
--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,14 +1,55 @@
+import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
-from datetime import datetime, timedelta
+from floodsystem.analysis import polyfit
+from floodsystem.stationdata import build_station_list
 
-def plot_water_levels(station, dates, levels):
-    plt.plot(dates,levels)
 
-    plt.xlabel('Date')
-    plt.ylabel('Water level (m)')
+#dates = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+#levels = [1,2,4,5,7,9,12,15,14,13,10,9,8,7,6,5,4,3]
+#p = 6
+
+#stations = build_station_list()
+#station = stations[0]
+
+
+#print(len(dates))
+#print(len(levels))
+
+def plot_water_level_with_fit(station, dates, levels, p):
+
+
+    dates_x = matplotlib.dates.date2num(dates)
+    poly = polyfit(dates_x, levels, p)
+    poly_y = poly(dates_x)
+
+    plt.plot(dates, levels, label='$Data$')
+    plt.plot(dates, poly_y, label='$Poly$')
+    plt.xlabel("$Time$")
+    plt.ylabel("$Water Level$")
     plt.xticks(rotation=45);
-    plt.title(station.name)
-
-    plt.tight_layout()
-
+    plt.legend()
     plt.show()
+    return
+
+#print(plot_water_level_with_fit(station, dates, levels, p))
+
+
+
+
+
+
+#import matplotlib.pyplot as plt
+#from datetime import datetime, timedelta
+
+#def plot_water_levels(station, dates, levels):
+#    plt.plot(dates,levels)
+
+#    plt.xlabel('Date')
+#    plt.ylabel('Water level (m)')
+#    plt.xticks(rotation=45);
+#    plt.title(station.name)
+
+#    plt.tight_layout()
+
+#    plt.show()
