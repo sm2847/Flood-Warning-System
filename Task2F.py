6e5d7f33bade48e504b075dff27d3d852ac888b5
Shanjeev Mathialagan <sm2847@cam.ac.uk>
2024-02-19 11:38:49 +0000

    2F

--- Task2F.py
+++ Task2F.py
@@ -4,25 +4,11 @@ import datetime
 import matplotlib.pyplot as plt
 from floodsystem.datafetcher import fetch_measure_levels
 from plot import plot_water_level_with_fit
-<<<<<<< HEAD
-from floodsystem.stationdata import build_station_list
-=======
 from floodsystem.stationdata import build_station_list, update_water_levels
->>>>>>> 80441f263c697c5117b32f7911f2000c276ae60c
 from floodsystem.flood import stations_highest_rel_level
 
 
 stations = build_station_list()
-<<<<<<< HEAD
-#top_five_risks =
-
-data = stations_highest_rel_level(stations, 5)
-
-
-
-
-
-=======
 update_water_levels(stations)
 
 def run():
@@ -36,18 +22,6 @@ def run():
 if __name__ == "__main__":
     print("*** Task 2F: CUED Part IA Flood Warning System ***")
     run()
->>>>>>> 80441f263c697c5117b32f7911f2000c276ae60c
-
-
-
-
-
-
-
-
-
-
-
 
 #print(stations_highest_rel_level(stations, 0))
 
